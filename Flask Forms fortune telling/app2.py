0597faffc9fee963d09203d3a2bed5f3267c690c
aliciaga24-meet <aliciagenson95@gmail.com>
2024-07-18 11:18:56 +0300

    Update app2.py

--- Flask Forms fortune telling/app2.py
+++ Flask Forms fortune telling/app2.py
@@ -22,8 +22,7 @@ def fortune(birth_month):
         "be happy.",
         "work hard.",
         "be who you are.",
-        "live love lught",
-        "Adventure awaits you this month.",
+        "live love lught"
         "yippiee ",
         "Love is in the air."
     ]
